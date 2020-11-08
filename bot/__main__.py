523c8ed7ed176d00f72c56e3cf17b0d013bdaba7
trancanh1102 <40576239+trancanh1102@users.noreply.github.com>
2020-11-09 04:52:42 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,7 +62,7 @@ def restart(update, context):
 @run_async
 def ping(update, context):
     start_time = int(round(time.time() * 1000))
-    reply = sendMessage("Starting Ping", context.bot, update)
+    reply = sendMessage("Đang kiểm tra thời giản phản hồi đến máy chủ!", context.bot, update)
     end_time = int(round(time.time() * 1000))
     editMessage(f'{end_time - start_time} ms', reply)
 
