c6e11f621cb4d67fa4374a28338e4ac991552ae3
trancanh1102 <40576239+trancanh1102@users.noreply.github.com>
2020-11-09 04:54:46 +0700

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -53,13 +53,13 @@ class AriaDownloadHelper(DownloadHelper):
     def __onDownloadPause(self, api, gid):
         LOGGER.info(f"onDownloadPause: {gid}")
         dl = getDownloadByGid(gid)
-        dl.getListener().onDownloadError('Download stopped by user!')
+        dl.getListener().onDownloadError('Người dùng đã dừng tải xuống!')
 
     @new_thread
     def __onDownloadStopped(self, api, gid):
         LOGGER.info(f"onDownloadStop: {gid}")
         dl = getDownloadByGid(gid)
-        if dl: dl.getListener().onDownloadError('Download stopped by user!')
+        if dl: dl.getListener().onDownloadError('Người dùng đã dừng tải xuống!')
 
     @new_thread
     def __onDownloadError(self, api, gid):
